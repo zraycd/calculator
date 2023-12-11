f320702b6c0335f99cb42b8d9b29715494a34c8d
zraycd <vainiusvjg@gmail.com>
2023-12-11 12:11:09 +0200

    add functional option buttons

--- script.js
+++ script.js
@@ -34,11 +34,23 @@ function operate(operator, a, b) {
 const display = document.querySelector('#display')
 let currentDisplay = ''
 document.querySelector('#nums').addEventListener('click', (event) => {
+    display.innerText = currentDisplay += event.target.innerText
+})
+document.querySelector('#operators').addEventListener('click', (event) => {
     currentDisplay += event.target.innerText
     display.innerText = currentDisplay
 })
-document.querySelector('#operators').addEventListener('click', (event) => {
-    currentDisplay += ' ' + event.target.innerText + ' '
-    display.innerText = currentDisplay
+document.querySelector('#options').addEventListener('click', (event) => {
+    if (event.target.innerText === 'Delete') {
+        if (currentDisplay < '10') {
+            display.innerText = currentDisplay = ''
+        } else {
+            let arr = currentDisplay.split('')
+            arr.pop()
+            display.innerText = currentDisplay = arr.join('')
+        }
+    } else if (event.target.innerText === 'Clear') {
+        display.innerText = currentDisplay = ''
+    }
 })
 
